388cac4aa0e02cf8f3aa7404264ad40e4729f2a2
ljm <794384313@qq.com>
2018-10-10 15:08:52 +0800

    添加抽奖draw的POJO类

---
+++ src/main/java/com/qilixxx/shareAct/domain/mapper/DrawMapper.java
@@ -0,0 +1,10 @@
+package com.qilixxx.shareAct.domain.mapper;
+
+
+import com.qilixxx.shareAct.domain.model.Draw;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+@Repository
+public interface DrawMapper extends Mapper<Draw> {
+}
